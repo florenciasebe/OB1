ff2f76b415444d2af5b552a8fee576eb15e7ee7b
florenciasebe <108202332+florenciasebe@users.noreply.github.com>
2022-10-26 21:51:45 -0300

    l

---
+++ JS/obligatorio.js
@@ -0,0 +1,42 @@
+inicio()
+
+function inicio (){
+    ocultarTodo();
+    document.querySelector("#menuPrincipal").style.display="block"
+}
+
+function ocultarTodo(){
+    document.querySelector("#botonInicio").style.display="none"
+    document.querySelector("#menuPrincipal").style.display="none"
+    document.querySelector("#sectorHomero").style.display="none"
+    document.querySelector("#sectorLisa").style.display="none"
+    document.querySelector("#sectorBart").style.display="none"
+}
+
+//escuchas de botones
+document.querySelector("#btnHomero").addEventListener("click", mostrarHomero);
+document.querySelector("#btnLisa").addEventListener("click", mostrarLisa);
+document.querySelector("#btnBart").addEventListener("click", mostrarBart);
+document.querySelector("#btnVolver").addEventListener("click", volver);
+
+function mostrarHomero(){
+    ocultarTodo()
+    document.querySelector("#sectorHomero").style.display="block"
+    document.querySelector("#botonInicio").style.display="block"
+}
+
+function volver(){
+    inicio()
+}
+
+function mostrarLisa(){
+    ocultarTodo()
+    document.querySelector("#sectorLisa").style.display="block"
+    document.querySelector("#botonInicio").style.display="block"
+}
+
+function mostrarBart(){
+    ocultarTodo()
+    document.querySelector("#sectorBart").style.display="block"
+    document.querySelector("#botonInicio").style.display="block"
+}
