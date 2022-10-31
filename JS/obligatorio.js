635cfea5ca788ba3bebb93cd2ac00981ce675ea1
florenciasebe <108202332+florenciasebe@users.noreply.github.com>
2022-10-31 20:12:47 -0300

    g

--- JS/obligatorio.js
+++ JS/obligatorio.js
@@ -19,6 +19,7 @@ function ocultarTodo(){
     document.querySelector("#divManifiesto").style.display="none"
     document.querySelector("#divCargasPeligrosas").style.display="none"
     document.querySelector("#divSalir").style.display="none"
+    document.querySelector("#divEstadisticas").style.display="none"
     //agregar HABILITAR IMPORTADORES
 }
 
@@ -31,6 +32,9 @@ document.querySelector("#btnManifiesto").addEventListener("click", manifiesto);
 document.querySelector("#btnCargaPeligrosa").addEventListener("click", cargaPeligrosa);
 document.querySelector("#btnLogout").addEventListener("click", logout);
 document.querySelector("#btnInicioSesion").addEventListener("click", iniciarSesion);
+document.querySelector("#btnEstadisticas").addEventListener("click", mostrarEstadisticas);
+document.querySelector("#btnNuevaSolicitud").addEventListener("click", nuevaSolicitud);
+document.querySelector("#btnSolicitudespendientes").addEventListener("click", solicitudesPendientes);
 
 function iniciarSesion(){
     if (document.querySelector("#slcElegirUsuario").value == "e") {
@@ -45,43 +49,59 @@ function iniciarSesion(){
     }
 }
 
+function logout (){
+    ocultarTodo()
+    inicio()
+}
+
+// FPANTALLAS IMPORTADORES//
+
+function mostrarEstadisticas (){
+    ocultarTodo()
+    document.querySelector("#divMenuImportador").style.display="block"
+    document.querySelector("#divEstadisticas").style.display="block"
+}
+function nuevaSolicitud (){
+    ocultarTodo()
+    document.querySelector("#divMenuImportador").style.display="block"
+    document.querySelector("#divNewSolicitud").style.display="block"
+}
+function solicitudesPendientes (){
+    ocultarTodo()
+    document.querySelector("#divMenuImportador").style.display="block"
+    document.querySelector("#divSolicitudesPendientes").style.display="block"
+}
+
+// FPANTALLAS EMPRESAS // 
 function crearViaje (){
     ocultarTodo()
     document.querySelector("#divMenuEmpresa").style.display="block"
     document.querySelector("#divNewViajeBuque").style.display="block"
 }
-
 function asignarSolicitud (){
     ocultarTodo()
     document.querySelector("#divMenuEmpresa").style.display="block"
     document.querySelector("#divAsignacionViaje").style.display="block"
 }
-
 function rollover (){
     ocultarTodo()
     document.querySelector("#divMenuEmpresa").style.display="block"
     document.querySelector("#divRollover").style.display="block"
 }
-
 function manifiesto(){
     ocultarTodo()
     document.querySelector("#divMenuEmpresa").style.display="block"
     document.querySelector("#divManifiesto").style.display="block"
 }
-
 /*function habilitarImportadores (){
     ocultarTodo()
     document.querySelector("#divMenuEmpresa").style.display="block"
     document.querySelector("#divRollover").style.display="block"
 }*/
-
 function cargaPeligrosa (){
     ocultarTodo()
     document.querySelector("#divMenuEmpresa").style.display="block"
     document.querySelector("#divCargasPeligrosas").style.display="block"
 }
 
-function logout (){
-    ocultarTodo()
-    inicio()
-}
+
