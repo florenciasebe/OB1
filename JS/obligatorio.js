d191f1fdd64677924615d087ad639bb0e046cdc7
florenciasebe <108202332+florenciasebe@users.noreply.github.com>
2022-10-31 18:21:08 -0300

    modificacion de js - ocultar divs

--- JS/obligatorio.js
+++ JS/obligatorio.js
@@ -5,46 +5,29 @@ function inicio (){
     document.querySelector("#divInicioSesion").style.display="block"
 }
 
+//ocultamos todos los divs (cada pantalla)
 function ocultarTodo(){
-    document.querySelector("#botonInicio").style.display="none"
-
-divFormRegistro
-divInicioSesion
-divMenuEmpresa
-divMenuImportador
-divNewSolicitud
-divSolicitudesPendientes
-divNewViajeBuque
-divAsignacionViaje
-divRollover
-divManifiesto
-dviCargasPeligrosas
+    document.querySelector("#divFormRegistro").style.display="none"
+    document.querySelector("#divInicioSesion").style.display="none"
+    document.querySelector("#divMenuEmpresa").style.display="none"
+    document.querySelector("#divMenuImportador").style.display="none"
+    document.querySelector("#divNewSolicitud").style.display="none"
+    document.querySelector("#divSolicitudesPendientes").style.display="none"
+    document.querySelector("#divNewViajeBuque").style.display="none"
+    document.querySelector("#divAsignacionViaje").style.display="none"
+    document.querySelector("#divRollover").style.display="none"
+    document.querySelector("#divManifiesto").style.display="none"
+    document.querySelector("#divCargasPeligrosas").style.display="none"
 }
 
 //escuchas de botones
-document.querySelector("#btnHomero").addEventListener("click", mostrarHomero);
-document.querySelector("#btnLisa").addEventListener("click", mostrarLisa);
-document.querySelector("#btnBart").addEventListener("click", mostrarBart);
-document.querySelector("#btnVolver").addEventListener("click", volver);
+document.querySelector("#btnCrearViaje").addEventListener("click", crearViaje);
+document.querySelector("#btnAsignarSolicitud").addEventListener("click", asiganSolicitud);
+document.querySelector("#btnRollover").addEventListener("click", rollover);
+document.querySelector("#btnManifiesto").addEventListener("click", manifiesto);
+document.querySelector("#btnHabilitarImportadores").addEventListener("click", habilitarImportadores);
+document.querySelector("#btnCargaPeligrosa").addEventListener("click", cargaPeligrosa);
+document.querySelector("#btnLogoutEmpresa").addEventListener("click", logoutEmpresa);
+document.querySelector("#btnInicioSesion").addEventListener("click", iniciarSesionEmpresa);
 
-function mostrarHomero(){
-    ocultarTodo()
-    document.querySelector("#sectorHomero").style.display="block"
-    document.querySelector("#botonInicio").style.display="block"
-}
 
-function volver(){
-    inicio()
-}
-
-function mostrarLisa(){
-    ocultarTodo()
-    document.querySelector("#sectorLisa").style.display="block"
-    document.querySelector("#botonInicio").style.display="block"
-}
-
-function mostrarBart(){
-    ocultarTodo()
-    document.querySelector("#sectorBart").style.display="block"
-    document.querySelector("#botonInicio").style.display="block"
-}
