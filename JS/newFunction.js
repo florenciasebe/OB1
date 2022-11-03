c24bf1154add67dd71dff473b36c0e2a879f3a04
florenciasebe <108202332+florenciasebe@users.noreply.github.com>
2022-11-03 12:14:36 -0300

    varios

--- JS/newFunction.js
+++ JS/newFunction.js
@@ -1,13 +1,17 @@
 //VALIDAR INICIO DE SESION 
+// escucho slc , usuario y clave cuando apreto btnIniciarSesion 
+//
+// si slc == "" Entonces alert ("Se debe elegir un tipo de usuario")
+//
+//Ver como esamblar esto con la funcion de Iniciar sesion
+// si slc == "e" Entonces validar usuario y clave vs el array de empresa
+//              si usuario o clave NO existe en el array Entonces alert ("usuario o clave incorrecta, intentelo nuevamete")
+//              sino inicioSesionEmpresa (armar function y borrar la funcition genreal - ver donde afecta antes de hacer los cambios)
+//
+// si slc == "i" Entonces validar usuario y clave vs el array de importador
+//              si usuario o clave NO existe en el array Entonces alert ("usuario o clave incorrecta, intentelo nuevamete")
+//              sino inicioSesionImportador (armar function y borrar la funcition genreal - ver donde afecta antes de hacer los cambios)
 
-
-
-
-// VALIDACION DE SLC //
-
-// VALIDACION DE USUARIO Y CLAVE // 
-
-// FORMULARIO DE REGISTRO - VALIDACIONES - CONFIRMAR REGISTRO //
 
 
 // LEVANTAR DATOS DEL INICIO DE SESION //
